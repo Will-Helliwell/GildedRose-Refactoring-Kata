3ef4946d43f247f976fdd31b617993a8dfc8b73b
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-10 12:30:15 +0000

    made context-setting in tests more readable, shorter lines

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -5,7 +5,9 @@ describe GildedRose do
   describe "#update_quality" do
 
     it "does not change the names of items" do
-      items = [Item.new("foo", 0, 0), Item.new("Aged Brie", 25, 25), Item.new("Sulfuras, Hand of Ragnaros", 100, 50)]
+      items = [Item.new("foo", 0, 0)]
+      items << Item.new("Aged Brie", 25, 25)
+      items << Item.new("Sulfuras, Hand of Ragnaros", 100, 50)
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
       expect(items[1].name).to eq "Aged Brie"
@@ -13,7 +15,9 @@ describe GildedRose do
     end
 
     context "given Sulfuras, Hand of Ragnaros" do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 100, 50), Item.new("Sulfuras, Hand of Ragnaros", 0, 0), Item.new("Sulfuras, Hand of Ragnaros", 10, 10)]
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 100, 50)]
+      items << Item.new("Sulfuras, Hand of Ragnaros", 0, 0)
+      items << Item.new("Sulfuras, Hand of Ragnaros", 10, 10)
       GildedRose.new(items).update_quality()
       it "does not change the sell_in" do
         expect(items[0].sell_in).to eq(100)
