c823042fc5bd2ade664d9d6f477ed3a8fa7ddee8
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-10 11:35:47 +0000

    add test for Sulfuras sell_in not changing

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -12,6 +12,14 @@ describe GildedRose do
       expect(items[2].name).to eq "Sulfuras, Hand of Ragnaros"
     end
 
+    context "given Sulfuras, Hand of Ragnaros" do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 100, 50)]
+      GildedRose.new(items).update_quality()
+      it "does not change the sell_in" do
+        expect(items[0].sell_in).to eq(100)
+      end
+    end
+
   end
 
 end
