ca513f9a5ad4f6e712cbc8c2a842d528adab33d1
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-11 17:12:59 +0000

    backstage pass tests now passing. All existing tests now green

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -11,13 +11,21 @@ class GildedRose
         when "Sulfuras, Hand of Ragnaros"
           break
         when "Aged Brie"
-          item.sell_in -= 1
           item.quality += 1 if item.quality < 50
+          item.sell_in -= 1
         when "Backstage passes to a TAFKAL80ETC concert"
+          if item.sell_in <= 0
+            item.quality = 0
+          elsif item.sell_in >= 1 && item.sell_in <= 5
+            item.quality += 3
+          elsif item.sell_in >= 6 && item.sell_in <= 10
+            item.quality += 2
+          elsif item.sell_in > 10
+            item.quality += 1
+          end
+          item.quality = 50 if item.quality > 50
           item.sell_in -= 1
-
         else
-          item.sell_in -= 1
           if item.sell_in > 0 && item.quality > 0
             item.quality -=1
           elsif item.sell_in <= 0 && item.quality == 1
@@ -25,6 +33,7 @@ class GildedRose
           elsif item.sell_in <= 0 && item.quality > 0
             item.quality -= 2
           end
+          item.sell_in -= 1
       end
     end
 
