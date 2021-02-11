fb0285bfda02ba103af99502a7c5e0413b1c4dbe
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-11 17:18:48 +0000

    misc item if statement logic now similar to backstage pass

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -28,11 +28,10 @@ class GildedRose
         else
           if item.sell_in > 0 && item.quality > 0
             item.quality -=1
-          elsif item.sell_in <= 0 && item.quality == 1
-            item.quality -= 1
           elsif item.sell_in <= 0 && item.quality > 0
             item.quality -= 2
           end
+          item.quality = 0 if item.quality < 0
           item.sell_in -= 1
       end
     end
