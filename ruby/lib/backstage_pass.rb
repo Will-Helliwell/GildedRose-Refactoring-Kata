b5b1b06403cdc103caecda63cd5a7e4ce7e2c326
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-13 14:08:40 +0000

    backstage pass update_quality tests now passing

--- ruby/lib/backstage_pass.rb
+++ ruby/lib/backstage_pass.rb
@@ -1,4 +1,17 @@
 require_relative './item.rb'
 
 class BackstagePass < Item
+
+  def update_quality
+    if self.sell_in <= 0
+      self.quality = Item::MIN_QUALITY
+    elsif self.sell_in.between?(1, 5)
+      self.quality += 3
+    elsif self.sell_in.between?(6, 10)
+      self.quality += 2
+    elsif self.sell_in > 10
+      self.quality += 1
+    end
+    self.quality = Item::MAX_QUALITY if self.quality > Item::MAX_QUALITY
+  end
 end
