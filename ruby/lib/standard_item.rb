c0d6a3c45392abd9ea367e56265b81c1e43bc24f
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-13 12:58:24 +0000

    update_quality tests now passing for StandardItem subclass

--- ruby/lib/standard_item.rb
+++ ruby/lib/standard_item.rb
@@ -1,4 +1,13 @@
 require_relative './item.rb'
 
 class StandardItem < Item
+
+  def update_quality
+    if self.sell_in.positive? && self.quality > Item::MIN_QUALITY
+      self.quality -= 1
+    elsif self.sell_in <= 0 && self.quality > Item::MIN_QUALITY
+      self.quality -= 2
+    end
+    self.quality = Item::MIN_QUALITY if self.quality < Item::MIN_QUALITY
+  end
 end
--- ruby/spec/standard_item_spec.rb
+++ ruby/spec/standard_item_spec.rb
@@ -21,19 +21,19 @@ describe StandardItem do
       end
     end
     it 'does not reduce the quality below Item::MIN_QUALITY' do
-      expect(standard_item_1.quality).to eq(Item::MIN_QUALITY)
-      expect(standard_item_2.quality).to eq(Item::MIN_QUALITY)
+      expect(@standard_item_1.quality).to eq(Item::MIN_QUALITY)
+      expect(@standard_item_2.quality).to eq(Item::MIN_QUALITY)
     end
     it 'reduces the quality by 1 when there are 1 or more days left to sell (and quality > 0)' do
-      expect(standard_item_3.quality).to eq(0)
-      expect(standard_item_4.quality).to eq(49)
+      expect(@standard_item_3.quality).to eq(0)
+      expect(@standard_item_4.quality).to eq(49)
     end
     it 'reduces the quality by 2 when sell_in has reached zero or below (and quality > 1)' do
-      expect(standard_item_5.quality).to eq(8)
-      expect(standard_item_6.quality).to eq(8)
+      expect(@standard_item_5.quality).to eq(8)
+      expect(@standard_item_6.quality).to eq(8)
     end
     it 'reduces the quality by 1 when sell_in has reached zero or below (and quality == 1)' do
-      expect(standard_item_7.quality).to eq(0)
+      expect(@standard_item_7.quality).to eq(0)
     end
   end
 end
