30549a3a946231fb04b1453173d35a63347980b9
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-13 13:36:58 +0000

    comnjured upodate quality tests now passing

--- ruby/lib/conjured.rb
+++ ruby/lib/conjured.rb
@@ -1,4 +1,13 @@
 require_relative './item.rb'
 
 class Conjured < Item
+
+  def update_quality
+    if self.sell_in.positive? && self.quality > Item::MIN_QUALITY
+      self.quality -= 2
+    elsif self.sell_in <= 0 && self.quality > Item::MIN_QUALITY
+      self.quality -= 4
+    end
+    self.quality = Item::MIN_QUALITY if self.quality < Item::MIN_QUALITY
+  end
 end
