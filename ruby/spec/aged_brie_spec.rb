5a08e58e73bd8d4b4131b9dc7c1d0739ac2982fb
Will-Helliwell <wj.helliwell@gmail.com>
2021-02-13 12:02:06 +0000

    transfer tests to aged_brie subclass - failing

--- ruby/spec/aged_brie_spec.rb
+++ ruby/spec/aged_brie_spec.rb
@@ -8,10 +8,14 @@ describe AgedBrie do
   end
 
   describe "#update_quality" do
-    let(:brie_1) {AgedBrie.new('Aged Brie', 25, 25)}
-    let(:brie_2) {AgedBrie.new('Aged Brie', 9, 0)}
-    let(:brie_3) {AgedBrie.new('Aged Brie', 0, 49)}
-    let(:brie_4) {AgedBrie.new('Aged Brie', 25, Item::MAX_QUALITY)}
+    before(:all) do
+    items = []
+    items << @brie_1 = AgedBrie.new('Aged Brie', 25, 25)
+    items << @brie_2 = AgedBrie.new('Aged Brie', 9, 0)
+    items << @brie_3 = AgedBrie.new('Aged Brie', 0, 49)
+    items << @brie_4 = AgedBrie.new('Aged Brie', 25, Item::MAX_QUALITY)
+    items.each{ |item| item.update_quality }
+    end
     it 'increases the quality by 1 when quality < Item::MAX_QUALITY' do
       expect(brie_1.quality).to eq(26)
       expect(brie_2.quality).to eq(1)
